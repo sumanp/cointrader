febe77a0fb6918462d04df8b5c45cd3f71da9adf
sumanp <sumanpuri55@gmail.com>
2021-05-26 00:32:54 +0530

    Added ability to unsubscribe from trade feed

--- lib/cointrader/exchanges.ex
+++ lib/cointrader/exchanges.ex
@@ -1,9 +1,14 @@
-defmodule Cointrader.Exchanges do #Exchange level abstracted public functions
+defmodule Cointrader.Exchanges do # Exchange context public functions
   alias Cointrader.{Product, Trade}
 
   @spec subscribe(Product.t()) :: :ok | {:error, term()}
   def subscribe(product) do
     Phoenix.PubSub.subscribe(Cointrader.PubSub, topic(product))
+  end
+
+  @spec unsubscribe(Product.t()) :: :ok | {:error, term()}
+  def unsubscribe(product) do
+    Phoenix.PubSub.unsubscribe(Cointrader.PubSub, topic(product))
   end
 
   @spec broadcast(Trade.t()) :: :ok | {:error, term()}
