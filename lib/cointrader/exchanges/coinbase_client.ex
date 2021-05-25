2581cd55e99e2f36861e60231c1dbe59c41607ad
sumanp <sumanpuri55@gmail.com>
2021-05-26 00:09:06 +0530

    Broadcast coinbase stream to PubSub

--- lib/cointrader/exchanges/coinbase_client.ex
+++ lib/cointrader/exchanges/coinbase_client.ex
@@ -1,5 +1,5 @@
 defmodule Cointrader.Exchanges.CoinbaseClient do #callback module (specific part)
-  alias Cointrader.{Trade, Product}
+  alias Cointrader.{Trade, Product, Exchanges}
   alias Cointrader.Exchanges.Client
   require Client
 
@@ -24,10 +24,8 @@ defmodule Cointrader.Exchanges.CoinbaseClient do #callback module (specific part
 
   @impl true
   def handle_ws_message(%{"type" => "ticker"}=msg, state) do
-      msg
-      |> message_to_trade()
-      |> IO.inspect(label: "trade")
-
+    {:ok, trade } = message_to_trade(msg) # crash process if pattern match fails
+    Exchanges.broadcast(trade)
     {:noreply, state}
   end
 
@@ -40,16 +38,18 @@ defmodule Cointrader.Exchanges.CoinbaseClient do #callback module (specific part
   @spec message_to_trade(map) :: {:ok, Trade.t()} | {:error, any()}
   def message_to_trade(msg) do
     with :ok <- validate_required(msg, ["product_id", "time", "price", "last_size"]),
-      {:ok, traded_at, _} <- DateTime.from_iso8601(msg["time"])
-    do
-    currency_pair = msg["product_id"]
-    Trade.new(
-      product: Product.new(exchange_name(), currency_pair),
-      price: msg["price"],
-      volume: msg["last_size"],
-      traded_at: traded_at
+      {:ok, traded_at, _} <- DateTime.from_iso8601(msg["time"]) do
 
-    )
+      currency_pair = msg["product_id"]
+      {
+        :ok,
+        Trade.new(
+          product: Product.new(exchange_name(), currency_pair),
+          price: msg["price"],
+          volume: msg["last_size"],
+          traded_at: traded_at
+        )
+      }
     else
       {:error, _reason}=error -> error
     end
