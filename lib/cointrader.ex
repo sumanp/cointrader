9ae58657fabecb734487e8d4de535bc7a1b088e1
sumanp <sumanpuri55@gmail.com>
2021-05-26 00:34:56 +0530

    Expose subscribe/unsubscribe API at the top level with delegates

--- lib/cointrader.ex
+++ lib/cointrader.ex
@@ -6,4 +6,10 @@ defmodule Cointrader do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+
+  defdelegate subscribe_to_trades(product),
+    to: Cointrader.Exchanges, as: :subscribe
+
+  defdelegate unsubscribe_from_trades(product),
+    to: Cointrader.Exchanges, as: :unsubscribe
 end
