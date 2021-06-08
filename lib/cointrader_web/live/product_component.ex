b771e90b08ab98365c95209187dffda2b81018ae
sumanp <sumanpuri55@gmail.com>
2021-06-09 01:54:41 +0530

    Refactored dashboard to use stateless LiveComponent

---
+++ lib/cointrader_web/live/product_component.ex
@@ -0,0 +1,44 @@
+defmodule CointraderWeb.ProductComponent do
+  use CointraderWeb, :live_component
+  import CointraderWeb.ProductHelpers
+
+  # optional callbacks for stateless components
+  # def mount(socket)
+  # def update(assigns, socket)
+
+  def render(assigns) do
+    ~L"""
+      <div class="product-component">
+        <div class="currency-container">
+          <img class="icon" src="<%= crypto_icon(@socket, @product) %>" />
+          <div class="crypto-name">
+            <%= crypto_name(@product) %>
+          </div>
+        </div>
+
+        <div class="price-container">
+          <ul class="fiat-symbols">
+            <%= for fiat <- fiat_symbols() do %>
+              <li class="
+              <%= if fiat_symbol(@product) == fiat, do: "active" %>
+                "><%= fiat %></li>
+            <% end %>
+          </ul>
+
+          <div class="price">
+            <%= @trade.price %>
+            <%= fiat_character(@product) %>
+          </div>
+        </div>
+
+        <div class="exchange-name">
+          <%= @product.exchange_name %>
+        </div>
+
+        <div class="trade-time">
+          <%= human_datetime(@trade.traded_at) %>
+        </div>
+      </div>
+    """
+  end
+end
