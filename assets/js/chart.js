6cff6cc8c5c7023d93096cc2fe024b39012ebe03
sumanp <sumanpuri55@gmail.com>
2021-06-12 20:10:11 +0530

    Use events instead of attributes to pass data

--- assets/js/chart.js
+++ assets/js/chart.js
@@ -2,54 +2,62 @@
 import _css from 'uplot/dist/uPlot.min.css'
 import uPlot from "uplot"
 
+let plotOpts = {
+  width: 200, height: 80, class: "chart-container",
+  cursor: { show: false }, select: { show: false }, legend: { show: false, },
+  scales: {
+  },
+  axes: [
+    {
+      show: false,
+    },
+    {
+      show: false,
+    }
+  ],
+  series: [
+    {},
+    {
+      size: 0,
+      width: 2,
+      stroke: "white",
+      fill: "rgb(45,85,150)",
+    },
+  ],
+};
+
+
 let ChartHook = {
   mounted() {
+    let productId = this.el.dataset.productId,
+      event = `new-trade:${productId}`,
+      self = this;
+
     this.trades = [];
-    this.plot = new uPlot(plotOptions(), [[], []], this.el);
+    this.plot = new uPlot(plotOpts, [[], []], this.el);
+    this.handleEvent(event, (payload) => self.handleNewTrade(payload));
   },
-  updated() {
-    let price = parseFloat(this.el.dataset.price),
-      timestamp = parseInt(this.el.dataset.tradedAt)
+  handleNewTrade(trade) {
+    let price = parseFloat(trade.price),
+      timestamp = trade.traded_at;
 
     this.trades.push({
       timestamp: timestamp, price: price
     });
 
-    if(this.trades.length > 20) {
-      this.trades.splice(0, 1)
+    if (this.trades.length > 20) {
+      this.trades.splice(0, 1);
     }
-    this.updateChart()
+
+    this.updateChart();
   },
+
 
   updateChart() {
     let x = this.trades.map(t => t.timestamp);
     let y = this.trades.map(t => t.price);
     this.plot.setData([x, y]);
   }
-}
-
-function plotOptions() {
-  return {
-    width: 200, height: 80, 
-    class: "chart-container",
-    cursor: {show: false}, 
-    select: {show: false}, 
-    legend: {show: false},
-    scales: {},
-    axes: [
-      {show: false},
-      {show: false}
-    ],
-    series: [
-      {},
-      {
-        size: 0,
-        width: 2,
-        stroke: "white",
-        fill: "rgb(45,85,150)",
-      },
-    ],
-  };
 }
 
 export { ChartHook }
--- lib/cointrader_web/live/product_component.ex
+++ lib/cointrader_web/live/product_component.ex
@@ -2,107 +2,120 @@ defmodule CointraderWeb.ProductComponent do
   use CointraderWeb, :live_component
   import CointraderWeb.ProductHelpers
 
-  # optional callbacks for stateless components
+  def update(%{trade: trade} = _assigns, socket) when not is_nil(trade) do
+    product_id = to_string(trade.product)
+    event_name = "new-trade:#{product_id}"
 
-  def update(%{trade: trade}=_assigns, socket) when not is_nil(trade) do
-    socket = assign(socket, :trade, trade)
+    socket =
+      socket
+      |> assign(:trade, trade)
+      |> push_event(event_name, to_event(trade))
+
     {:ok, socket}
   end
 
   def update(assigns, socket) do
     product = assigns.id
+
     socket =
       assign(socket,
         product: product,
         trade: Cointrader.get_last_trade(product),
         timezone: assigns.timezone
       )
+
     {:ok, socket}
   end
 
-  # optional callbacks close
-
-  def render(%{trade: trade}=assigns) when not is_nil(trade) do
+  def render(%{trade: trade} = assigns) when not is_nil(trade) do
     ~L"""
-      <div class="product-component">
-        <button class="remove" phx-click="remove-product"
-          phx-value-product-id="<%= to_string(@product)%>">X</button>
-        <div class="currency-container">
-          <img class="icon" src="<%= crypto_icon(@socket, @product) %>" />
-          <div class="crypto-name">
-            <%= crypto_name(@product) %>
-          </div>
-        </div>
-
-        <div class="price-container">
-          <ul class="fiat-symbols">
-            <%= for fiat <- fiat_symbols() do %>
-              <li class="
-              <%= if fiat_symbol(@product) == fiat, do: "active" %>
-                "><%= fiat %></li>
-            <% end %>
-          </ul>
-
-          <div class="price">
-            <%= @trade.price %>
-            <%= fiat_character(@product) %>
-          </div>
-        </div>
-
-        <div class="chart-component">
-          <div phx-hook="Chart"
-              id="product-chart-<%= to_string(@product) %>"
-              data-price="<%= @trade.price %>"
-              data-traded-at="<%= DateTime.to_unix(@trade.traded_at, :millisecond) %>"
-              phx-update="ignore"
-            >
-            <div class="chart-container"></div>
-          </div>
-        </div>
-
-        <div class="exchange-name">
-          <%= @product.exchange_name %>
-        </div>
-
-        <div class="trade-time">
-          <%= human_datetime(@trade.traded_at, @timezone) %>
+    <div class="product-component">
+      <button class="remove"
+              phx-click="remove-product"
+              phx-value-product-id="<%= to_string(@product) %>">X</button>
+      <div class="currency-container">
+        <img class="icon" src="<%= crypto_icon(@socket, @product) %>" />
+        <div class="crypto-name">
+          <%= crypto_name(@product) %>
         </div>
       </div>
+
+      <div class="price-container">
+        <ul class="fiat-symbols">
+          <%= for fiat <- fiat_symbols() do %>
+            <li class="
+            <%= if fiat_symbol(@product) == fiat, do: "active" %>
+              "><%= fiat %></li>
+          <% end %>
+       </ul>
+
+        <div class="price">
+          <%= @trade.price %>
+          <%= fiat_character(@product) %>
+        </div>
+      </div>
+
+      <div class="chart-component">
+        <div phx-hook="Chart"
+            id="product-chart-<%= to_string(@product) %>"
+            data-product-id="<%= to_string(@product) %>"
+            phx-update="ignore"
+            >
+          <div class="chart-container"></div>
+        </div>
+      </div>
+
+      <div class="exchange-name">
+        <%= @product.exchange_name %>
+      </div>
+
+      <div class="trade-time">
+        <%= human_datetime(@trade.traded_at, @timezone) %>
+      </div>
+    </div>
     """
   end
 
   def render(assigns) do
     ~L"""
-      <div class="product-component">
-        <div class="currency-container">
-          <img class="icon" src="<%= crypto_icon(@socket, @product) %>" />
-          <div class="crypto-name">
-            <%= crypto_name(@product) %>
-          </div>
-        </div>
-
-        <div class="price-container">
-          <ul class="fiat-symbols">
-            <%= for fiat <- fiat_symbols() do %>
-              <li class="
-              <%= if fiat_symbol(@product) == fiat, do: "active" %>
-                "><%= fiat %></li>
-            <% end %>
-          </ul>
-
-          <div class="price">
-            ...
-            <%= fiat_character(@product) %>
-          </div>
-        </div>
-
-        <div class="exchange-name">
-          <%= @product.exchange_name %>
-        </div>
-
-        <div class="trade-time">
+    <div class="product-component">
+      <div class="currency-container">
+        <img class="icon" src="<%= crypto_icon(@socket, @product) %>" />
+        <div class="crypto-name">
+          <%= crypto_name(@product) %>
         </div>
       </div>
+
+      <div class="price-container">
+        <ul class="fiat-symbols">
+          <%= for fiat <- fiat_symbols() do %>
+            <li class="
+            <%= if fiat_symbol(@product) == fiat, do: "active" %>
+              "><%= fiat %></li>
+          <% end %>
+      </ul>
+
+        <div class="price">
+          ...
+          <%= fiat_character(@product) %>
+        </div>
+      </div>
+
+      <div class="exchange-name">
+        <%= @product.exchange_name %>
+      </div>
+
+      <div class="trade-time">
+      </div>
+    </div>
     """
+  end
+
+  defp to_event(trade) do
+    %{
+      traded_at: DateTime.to_unix(trade.traded_at, :millisecond),
+      price: trade.price,
+      volume: trade.volume
+    }
   end
 end
