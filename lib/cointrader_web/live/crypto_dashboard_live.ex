2733d03309a7de066430f0c722d63caa62bf1778
sumanp <sumanpuri55@gmail.com>
2021-07-19 03:19:58 +0530

    Remove product list via handle params

--- lib/cointrader_web/live/crypto_dashboard_live.ex
+++ lib/cointrader_web/live/crypto_dashboard_live.ex
@@ -5,7 +5,7 @@ defmodule CointraderWeb.CryptoDashboardLive do #each concurrent user has their o
   alias CointraderWeb.Router.Helpers, as: Routes
 
   @impl true
-  def mount(params, _session, socket) do # entry point
+  def mount(_params, _session, socket) do # entry point
     socket =
       socket
       |> assign(products: [], filter_products: & &1, timezone: get_timezone_from_connection(socket))
@@ -14,7 +14,7 @@ defmodule CointraderWeb.CryptoDashboardLive do #each concurrent user has their o
   end
 
   @impl true
-  def handle_params(%{"product"=> product_ids}=params, _uri, socket) do # always invoked after mount; patch/push triggers the func
+  def handle_params(%{"product"=> product_ids}=_params, _uri, socket) do # always invoked after mount; patch/push triggers the func
     new_products = Enum.map(product_ids, &product_from_string/1)
     diff = List.myers_difference(socket.assigns.products, new_products)
     products_to_remove = diff |> Keyword.get_values(:del) |> List.flatten()
@@ -74,10 +74,15 @@ defmodule CointraderWeb.CryptoDashboardLive do #each concurrent user has their o
   def handle_event("remove-product", %{"product-id" => product_id} = _params, socket) do
     product = product_from_string(product_id)
     Cointrader.unsubscribe_from_trades(product)
-    socket =
-      socket
-      |> update(:products, &List.delete(&1, product))
-      |> update_products_params()
+
+    product_ids =
+      socket.assigns.products
+      |> Enum.map(&to_string/1)
+      |> Kernel.--([product_id]) # remove & update existing list
+
+    socket = push_patch(socket,
+      to: Routes.live_path(socket, __MODULE__, products: product_ids))
+
     {:noreply, socket}
   end
 
@@ -88,7 +93,7 @@ defmodule CointraderWeb.CryptoDashboardLive do #each concurrent user has their o
   end
 
   def remove_product(socket, product) do
-    Cointrader.unsubscribe_to_trades(product)
+    Cointrader.unsubscribe_from_trades(product)
     socket
     |> update(:products, &(&1 -- [product]))
   end
@@ -103,10 +108,5 @@ defmodule CointraderWeb.CryptoDashboardLive do #each concurrent user has their o
       %{"timezone" => tz} when not is_nil(tz) -> tz
       _ -> "UTC"
     end
-  end
-
-  defp update_products_params(socket) do
-    product_ids = Enum.map(socket.assigns.products, &to_string/1)
-    push_patch(socket, to: Routes.live_path(socket, __MODULE__, products: product_ids))
   end
 end
